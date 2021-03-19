95427cfdefa9fb3763308913a2afaa0b82b0d7ae
Ajay Thanki <akthanki5@gmail.com>
2021-03-19 19:49:00 +0530

    fixed responsive testimonial slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,42 +87,52 @@
   });
 
   // clients logo slider
-  $('.slider').slick({
+  $('.myslider').slick({
     centerMode: true,
+    centerPadding: '200px',
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
+    arrows: false,
     dots: true,
-    arrows: true,
-    responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 400,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
+    responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        centerPadding: '130px',
       }
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        centerPadding: '80px',
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        centerPadding: '150px',
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        centerPadding: '100px',
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        centerPadding: '50px',
+      }
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        centerMode: false,
+      }
+    },
     ]
   });
   
